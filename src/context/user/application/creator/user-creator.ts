f6fcf92b3571f2c9675bfdcc94d396a9420f4ceb
andrea-grupoformarte <desarrollador1@grupoformarte.edu.co>
2026-01-19 17:15:13 -0500

    feature: validate duplicated email

--- src/context/user/application/creator/user-creator.ts
+++ src/context/user/application/creator/user-creator.ts
@@ -2,6 +2,7 @@ import { Injectable, Inject } from '@nestjs/common';
 import { UserRepository } from '../../domain/userRepository';
 import { User, UserPrimitiveType } from '../../domain/user';
 import { IPasswordHasher } from '../../../shared/domain/IPasswordHasher';
+import { ConflictError } from '../../../shared/domain/errors/ConflictError';
 
 @Injectable()
 export class UserCreator {
@@ -11,6 +12,20 @@ export class UserCreator {
     ) { }
 
     async run(data: UserPrimitiveType): Promise<User> {
+        if (data.email) {
+            const userWithEmail = await this.userRepository.findByEmail(data.email);
+            if (userWithEmail) {
+                throw new ConflictError(`User with email ${data.email} already exists`);
+            }
+        }
+
+        if (data.cellPhone) {
+            const userWithPhone = await this.userRepository.findByCellPhone(data.cellPhone);
+            if (userWithPhone) {
+                throw new ConflictError(`User with cell phone ${data.cellPhone} already exists`);
+            }
+        }
+
         if (data.password) {
             data.password = await this.passwordHasher.hash(data.password);
         }
